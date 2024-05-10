2ecbef0788dd4e75d8156edd9d39c32a019073c2
sandramerc <144231118+sandramercadal@users.noreply.github.com>
2024-05-10 12:35:06 +0100

    Added the Typwriter JS plugin and basic form handling JS.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function generateBook(event) {
+  event.preventDefault();
 
+  new Typewriter("#book-result", {
+    strings: "One Day by David Nicholls",
+    autoStart: true,
+    cursor: null,
+    delay: 40,
+  });
+}
+
+let bookFormElement = document.querySelector("#book-generator-form");
+bookFormElement.addEventListener("submit", generateBook);
