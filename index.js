453d6bab7d66d035a1689b46922316b8bd62c217
sandramerc <144231118+sandramercadal@users.noreply.github.com>
2024-05-12 08:39:17 +0100

    Removed the dummy data and trying to call real data.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function displayBook(response) {
-  //response.date.answer will have the answer in it
+  //response.data.answer will have the answer in it
   new Typewriter("#book-result", {
     strings: response.data.answer,
     autoStart: true,
@@ -14,14 +14,14 @@ function generateBook(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "b88b1f146af7a33abtdd4oddc5070ff6";
-  let prompt = `User instructions: Generate 2 book recommendations based on ${instructionsInput.value}`;
+  let prompt = `User instructions: Generate 3 book recommendations based on ${instructionsInput.value}`;
   let context =
-    "You are a well read librarian who loves to suggest more books to read. You need to recommend 2 book titles based on what the reader has read before this is the user instructions input. Please provide the title of the book and the author. Also a brief synopsis of what each book is about too in basic HTML. At the end on a new line underneath write 'Happy reading - AI Librarian' in bold";
+    "You are a well read librarian who loves to suggest more books to read. You need to recommend 3 book titles based on what the reader has read before this is the user instructions input. Please provide the title of the book and the author. Also a brief synopsis of what each book is about too in basic HTML. At the end on a new line underneath write 'Happy reading - AI Librarian' in bold";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   //make a call to the APi with Axios
   console.log("Here some your book recommendations");
-  axios.get(apiUrl).then(displaygenerateBook);
+  axios.get(apiUrl).then(displayBook);
 }
 
 let bookFormElement = document.querySelector("#book-generator-form");
