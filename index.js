90f475ed497d63115d77e736e5cd4806a0dbd0ee
sandramerc <144231118+sandramercadal@users.noreply.github.com>
2024-05-14 21:58:17 +0100

    Added 'the authors name' to ensure this was generated in the output.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function generateBook(event) {
   let apiKey = "b88b1f146af7a33abtdd4oddc5070ff6";
   let prompt = `User instructions: Generate 3 book recommendations based on ${instructionsInput.value}`;
   let context =
-    "Give me 3 book recommendations based on the booked entered in the prompt. Do not recommend the book the user entered in the prompt. Do not use the words title, book recommendations or book recommendation. The answer should be in html format. Use the ordered list tag to list the books. The brief synopsis of what each book is like should be 4 lines or less in basic HTML. Use the break line html tag to separate the title from the sinopsis. Do not use the paragraph tag. At the end add a break line html element <br /> and sign the recommendations from yourself as follows 'Happy reading - AI Librarian' in strong html element.";
+    "Give me 3 book recommendations and the authors name based on the booked entered in the prompt. Do not recommend the book the user entered in the prompt. Do not use the words title, book recommendations or book recommendation. The answer should be in html format. Use the ordered list tag to list the books. The brief synopsis of what each book is like should be 4 lines or less in basic HTML. Use the break line html tag to separate the title from the sinopsis. Do not use the paragraph tag. At the end add a break line html element <br /> and sign the recommendations from yourself as follows 'Happy reading - AI Librarian' in strong html element.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let bookResultElement = document.querySelector("#book-result");
